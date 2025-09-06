3e73ce152807a27cb5f0b667dd0428022da4831f
Koutsi <ckoutsiotas@gmail.com>
2025-09-06 16:05:36 +0300

    Tic-Tac-Toe game using React.js

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,13 @@
+import { TicTacToe } from "./Components/TicTacToe/TicTacToe.jsx";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div>
+     <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
